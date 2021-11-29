b3746a3d2bc6e8115082849184a0f3031a00c958
advancedlenard <ge26hab@mytum.de>
2021-11-30 01:39:43 +0800

    added the unit_test.cpp

---
+++ unit_test.cpp
@@ -0,0 +1,20 @@
+/********************************************************************************* 
+ 
+  * FileName:       unit_test.cpp
+  * Author:         Zichen Zhang, Mingshuai Li
+  * Version:        V1.00
+  * Date:           2021.11.29
+  * Description:    The implementation for the unit test functions
+  * Project:        The group project for the WS2021 course IN1503 Advanced Programming
+
+**********************************************************************************/
+
+
+#include "unit_test.hpp"
+
+
+void TestReadImage()
+{
+}
+
+
