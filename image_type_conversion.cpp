03b0bc841438fb3ea995d1fa750b353652bb9ca8
advancedlenard <ge26hab@mytum.de>
2021-11-30 02:26:09 +0800

    added the implementations of image type conversion functions

---
+++ image_type_conversion.cpp
@@ -0,0 +1,15 @@
+/********************************************************************************* 
+ 
+  * FileName:       image_type_conversion.cpp
+  * Author:         Zichen Zhang, Mingshuai Li
+  * Version:        V1.00
+  * Date:           2021.11.29
+  * Description:    The implementation for some functions related to image-type conversion
+  * Project:        The group project for the WS2021 course IN1503 Advanced Programming
+
+**********************************************************************************/
+
+
+#include "image_type_conversion.hpp"
+
+
