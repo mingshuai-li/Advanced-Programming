622bd948d7709f7fa8908b4809bdb57f3259eefe
advancedlenard <ge26hab@mytum.de>
2021-11-30 02:25:16 +0800

    added the imlpementations of image enhancement functions

---
+++ image_enhancement.cpp
@@ -0,0 +1,15 @@
+/********************************************************************************* 
+ 
+  * FileName:       image_enhancement.cpp
+  * Author:         Zichen Zhang, Mingshuai Li
+  * Version:        V1.00
+  * Date:           2021.11.29
+  * Description:    The implementation for the image enhancement algorithms
+  * Project:        The group project for the WS2021 course IN1503 Advanced Programming
+
+**********************************************************************************/
+
+
+#include "image_enhancement.hpp"
+
+
