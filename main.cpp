b383bbf8602a5b70a550e08fc6d4dacacba03f41
advancedlenard <ge26hab@mytum.de>
2021-11-30 01:38:59 +0800

    added the main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/********************************************************************************* 
+ 
+  * FileName:       main.cpp
+  * Author:         Zichen Zhang, Mingshuai Li
+  * Version:        V1.00
+  * Date:           2021.11.29
+  * Description:    The main file
+  * Project:        The group project for the WS2021 course IN1503 Advanced Programming
+
+**********************************************************************************/
+
+
+#include "image_io.hpp"
+
+
+int main(int argc, char** argv)
+{
+
+    cv::Mat image = ReadImage("NewYork.jpg");
+    DisplayImage(image, "New York!");
+    PrintImageInfo(image);
+    return 0;
+
+}
+
+
