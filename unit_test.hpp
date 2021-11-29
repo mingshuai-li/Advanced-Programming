50fd3ec7dc465da26a34e9f403f3320b1a381e84
advancedlenard <ge26hab@mytum.de>
2021-11-30 01:39:20 +0800

    added the header file for  unit test

---
+++ unit_test.hpp
@@ -0,0 +1,40 @@
+/********************************************************************************* 
+ 
+  * FileName:       unit_test.hpp
+  * Author:         Zichen Zhang, Mingshuai Li
+  * Version:        V1.00
+  * Date:           2021.11.29
+  * Description:    The header file for the unit test functions 
+  * Project:        The group project for the WS2021 course IN1503 Advanced Programming
+
+**********************************************************************************/
+
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "image_io.hpp"
+
+
+/*
+* Tests the ReadImage()
+*/
+void TestReadImage();
+
+
+/*
+* Tests the DisplayImage()
+*/
+void TestDisplayImage();
+
+
+/*
+* Tests the PrintImageInfo()
+*/
+void TestPrintImageInfo();
+
+
+
+
+
+
