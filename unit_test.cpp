93bebe47824c98b566943cacb7525217fe1f961c
advancedlenard <ge26hab@mytum.de>
2021-11-30 23:59:40 +0800

    added implementations for ConvertUchar2DoubleC1 and MapDouble2Uchar

--- unit_test.cpp
+++ unit_test.cpp
@@ -23,9 +23,32 @@ void TestImageIO()
 
 void TestConvertRGB2GrayScale()
 {
-  cv::Mat input_image = ReadImage("NewYork.jpg");
+    cv::Mat input_image = ReadImage("NewYork.jpg");
     cv::Mat output_image = ConvertRGB2GrayScale(input_image);
     DisplayImage(output_image, "New York!");
+}
+
+
+void TestConvertUchar2DoubleC1()
+{
+    cv::Mat input_image = ReadImage("NewYork.jpg");
+    cv::Mat grayscale_input_image = ConvertRGB2GrayScale(input_image);
+    PrintImageInfo(input_image);
+    cv::Mat output_image = ConvertUchar2DoubleC1(grayscale_input_image);
+    PrintImageInfo(output_image);
+}
+
+
+void TestMapDouble2Uchar()
+{
+    double n1 = 256.0;
+    double n2 = 253.0;
+    double n3 = -3.0;
+    uchar mapped_n1 = MapDouble2Uchar(n1);
+    uchar mapped_n2 = MapDouble2Uchar(n2);
+    uchar mapped_n3 = MapDouble2Uchar(n3);
+    std::cout << n1 << "   " << n2 << "   " << n3 << '\n';
+    std::cout << mapped_n1 << "   " << mapped_n2 << "   " << mapped_n3 << '\n';
 }
 
 
