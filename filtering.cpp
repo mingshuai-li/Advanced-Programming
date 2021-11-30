c65894a3cec21e86978cc45bc057f0e3525da9b1
advancedlenard <ge26hab@mytum.de>
2021-11-30 23:09:04 +0800

    added the filtering.cpp

---
+++ filtering.cpp
@@ -0,0 +1,15 @@
+/********************************************************************************* 
+ 
+  * FileName:       filtering.cpp
+  * Author:         Zichen Zhang, Mingshuai Li
+  * Version:        V1.00
+  * Date:           2021.11.29
+  * Description:    The implementation for the filtering algorithms
+  * Project:        The group project for the WS2021 course IN1503 Advanced Programming
+
+**********************************************************************************/
+
+
+#include "filtering.hpp"
+
+
