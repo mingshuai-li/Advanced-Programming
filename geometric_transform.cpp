d3edd17b3e8f223131c79102a6a78b32c7b54841
advancedlenard <ge26hab@mytum.de>
2021-12-01 17:57:46 +0800

    refactored functions

--- geometric_transform.cpp
+++ geometric_transform.cpp
@@ -12,56 +12,63 @@
 
 #include "geometric_transform.hpp"
 
-cv::Mat Resize(const cv::Mat& input_image, double kx, double ky)
-{  
+cv::Mat Resize(const cv::Mat& input_image, const double& height_scaling, const double& width_scaling)
+{
 
-  int row = cvRound(input_image.rows * kx), col = cvRound(input_image.cols * ky);
-	cv::Mat output_image(row, col, input_image.type());
+    int output_height = cvRound(input_image.rows * height_scaling);
+    int output_width = cvRound(input_image.cols * width_scaling);
+	  cv::Mat output_image(output_height, output_width, CV_8UC1);
 
+    int map_row_out_to_in = 0;
+    int map_col_out_to_in = 0;
 
-  for (int i = 0; i < row; i++)
-  {
-    for (int j = 0; j < col; j++)
+    for (int i = 0; i < output_height; i++)
     {
-      int x = static_cast<int>((i + 1) / kx + 0.5) - 1;
-      int y = static_cast<int>((j + 1) / ky + 0.5) - 1;
-      output_image.at<cv::Vec3b>(i, j) = input_image.at<cv::Vec3b>(x, y);
+        for (int j = 0; j < output_width; j++)
+        {
+            map_row_out_to_in = static_cast<int>((i + 1) / height_scaling + 0.5) - 1;
+            map_col_out_to_in = static_cast<int>((j + 1) / width_scaling + 0.5) - 1;
+            output_image.at<uchar>(i, j) = input_image.at<uchar>(map_row_out_to_in, map_col_out_to_in);
+        }
     }
-  }
 
-  return output_image;
+    return output_image;
 
 }
+
 
 cv::Mat Rotate(const cv::Mat& input_image, const double& angle)
 {
 
-	int len = (int)(sqrtf(pow(input_image.rows, 2) + pow(input_image.cols, 2)) + 0.5);
- 
-	cv::Mat output_image = cv::Mat::zeros(len, len, CV_8UC3);
-	double anglePI = angle * CV_PI / 180;
-	int xSm, ySm;
- 
-	for(int i = 0; i < output_image.rows; i++)
-  {
-		for(int j = 0; j < output_image.cols; j++)
-		{
-			xSm = (int)((i-output_image.rows/2)*cos(anglePI) - (j-output_image.cols/2)*sin(anglePI) + 0.5);
-			ySm = (int)((i-output_image.rows/2)*sin(anglePI) + (j-output_image.cols/2)*cos(anglePI) + 0.5);
-			xSm += input_image.rows / 2;
-			ySm += input_image.cols / 2;
- 
-			if(xSm >= input_image.rows || ySm >= input_image.cols || xSm <= 0 || ySm <= 0)
-      {
-				output_image.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 0);
-			}
-			else
-      {
-				output_image.at<cv::Vec3b>(i, j) = input_image.at<cv::Vec3b>(xSm, ySm);
-			}
-		}
-  }
+    int image_height = input_image.rows;
+    int image_width = input_image.cols;
+    int output_size = static_cast<int>(sqrtf(pow(input_image.rows, 2) + pow(input_image.cols, 2)) + 0.5);
 
+    cv::Mat output_image = cv::Mat::zeros(output_size, output_size, CV_8UC1);
 
-  return output_image;
+    double angle_in_rad = angle * PI / 180;
+    double sin_angle = sin(angle_in_rad);
+    double cos_angle = cos(angle_in_rad);
+
+    int map_row_out_to_in = 0;
+    int map_col_out_to_in = 0;
+
+    for (int i = 0; i < output_size; i++)
+    {
+        for (int j = 0; j < output_size; j++)
+        {
+            map_row_out_to_in = static_cast<int>((i - output_size / 2) * cos_angle - (j - output_size / 2) * sin_angle + 0.5);
+            map_col_out_to_in = static_cast<int>((i - output_size / 2) * sin_angle + (j - output_size / 2) * cos_angle + 0.5);
+            map_row_out_to_in += image_height / 2;
+            map_col_out_to_in += image_width / 2;
+
+            if (map_row_out_to_in < 0 || map_row_out_to_in >= image_height || map_col_out_to_in < 0 || map_col_out_to_in >= image_width)
+                output_image.at<uchar>(i, j) = static_cast<uchar>(0);
+            else
+                output_image.at<uchar>(i, j) = input_image.at<uchar>(map_row_out_to_in, map_col_out_to_in);
+        }
+    }
+
+    return output_image;
+
 }
