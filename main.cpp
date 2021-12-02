7405fede62fe4f473d700db49efdd135f64f846e
advancedlenard <ge26hab@mytum.de>
2021-12-03 07:01:04 +0800

    added initial menu

--- main.cpp
+++ main.cpp
@@ -19,7 +19,23 @@
 
 int main(int argc, char** argv)
 {
+
+    cv::Mat input_image = cv::imread("01.jpg");
+    cv::Mat output_image = ConvertRGB2GrayScale(input_image);
+    input_image = output_image;
+  
+    int choice = 0;
+    double para1 = 0.0;
+    double para2 = 0.0;
+    int kernel_height = 0;
+    int kernel_width = 0;
+
+    std::cout << "Welcome to use this image processing system(Version 1.00)!\n";
+    std::cout << "Which kind of operations would you like to perform?\n";
+    std::cout << "1.Image Enhancement\n2.Image Geometric Transform\n3.Filtering\n";;
+    std::cin >> choice;
     return 0;
+
 }
 
 
