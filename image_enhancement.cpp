9d19452de1037d59e0d845ffe6b5d6d8de242a9a
vmlims <mingshuai.li@tum.de>
2021-12-02 18:17:01 +0800

    correct a mistake

--- image_enhancement.cpp
+++ image_enhancement.cpp
@@ -3,7 +3,7 @@
   * FileName:       image_enhancement.cpp
   * Author:         Zichen Zhang, Mingshuai Li
   * Version:        V1.00
-  * Date:           2021.11.19
+  * Date:           2021.11.29
   * Description:    The implementation for the image enhancement algorithms
   * Project:        The group project for the WS2021 course IN1503 Advanced Programming
 
