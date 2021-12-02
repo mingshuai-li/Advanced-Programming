38378005327f89ca6b4bd7e5efd83c2e92f8bee1
vmlims <mingshuai.li@tum.de>
2021-12-02 18:17:12 +0800

    correct a mistake

--- geometric_transform.cpp
+++ geometric_transform.cpp
@@ -3,7 +3,7 @@
   * FileName:       geometric_transform.cpp
   * Author:         Zichen Zhang, Mingshuai Li
   * Version:        V1.00
-  * Date:           2021.11.19
+  * Date:           2021.11.29
   * Description:    The implementation for the geometric transform algorithms
   * Project:        The group project for the WS2021 course IN1503 Advanced Programming
 
