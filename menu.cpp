923b99ae05259c8aa9bb14659f1d51cd23ea4aa6
advancedlenard <ge26hab@mytum.de>
2021-12-24 20:02:08 +0800

    added the implementation of menu

---
+++ menu.cpp
@@ -0,0 +1,69 @@
+/********************************************************************************* 
+ 
+  * FileName:       menu.cpp
+  * Author:         Zichen Zhang, Mingshuai Li
+  * Version:        V2.00
+  * Date:           2021.12.23
+  * Description:    The implementation for the class Menu
+  * Project:        The group project for the WS2021 course IN1503 Advanced Programming
+
+**********************************************************************************/
+
+
+#include "menu.hpp"
+
+
+int Menu::MainMenu()
+{
+    int choice = 0;
+
+    std::cout << "Welcome to use this image processing system(Version 1.00)!\n";
+    std::cout << "Which kind of operations would you like to perform?\n";
+    std::cout << "1.Image Enhancement\n2.Image Geometric Transform\n3.Filtering\n";;
+    std::cin >> choice;
+
+    return choice;
+
+}
+
+
+int Menu::ImageEnhancementMenu()
+{
+    int choice = 0;
+
+    std::cout << "Which kind of Image Enhancement operations would you like to perform?\n";
+    std::cout << "1.Brightness Transform\n2.Inverse Transform\n3.Gamma Transform\n4.Log Transform\n";
+    std::cout << "5.Normalization Transform\n6.Threshold Transform\n7.Window Transform\n";
+    std::cin >> choice;
+
+    return choice;
+
+}
+
+
+int Menu::GeometricTransformMenu()
+{
+    int choice = 0;
+
+    std::cout << "Which kind of Image Geometric Transform operations would you like to perform?\n";
+    std::cout << "1.Resize\n2.Rotate\n3.FlipLeftRight\n4.FlipUpDown\n";
+    std::cin >> choice;
+
+    return choice;
+
+}
+
+
+int Menu::FilteringMenu()
+{
+    int choice = 0;
+
+    std::cout << "Which kind of Filtering operations would you like to perform?\n";
+    std::cout << "1.Low Pass Filtering\n2.High Pass Filtering\n3.Band Pass Filtering\n4.Gaussian Filtering\n5.Laplacian Filtering\n";
+    std::cin >> choice;
+
+    return choice;
+
+}
+
+
