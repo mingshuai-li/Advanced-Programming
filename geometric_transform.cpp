a0799f4b06de8d22c3fc76feba2f74878156cc9a
advancedlenard <ge26hab@mytum.de>
2021-12-24 19:57:43 +0800

    set some variables as const

--- geometric_transform.cpp
+++ geometric_transform.cpp
@@ -2,9 +2,9 @@
  
   * FileName:       geometric_transform.cpp
   * Author:         Zichen Zhang, Mingshuai Li
-  * Version:        V1.00
-  * Date:           2021.11.29
-  * Description:    The implementation for the geometric transform algorithms
+  * Version:        V2.00
+  * Date:           2021.12.23
+  * Description:    The implementation for the class GeometricTransform
   * Project:        The group project for the WS2021 course IN1503 Advanced Programming
 
 **********************************************************************************/
@@ -13,11 +13,15 @@
 #include "geometric_transform.hpp"
 
 
-cv::Mat GeometricTransform::Resize(const cv::Mat& input_image, const double& height_scaling, const double& width_scaling)
+const double GeometricTransform::PI_ = 3.141592654;
+
+
+cv::Mat GeometricTransform::Resize(const cv::Mat& input_image, double height_scaling, double width_scaling)
 {  
 
-    int rows = cvRound(input_image.rows * height_scaling);
-    int cols = cvRound(input_image.cols * width_scaling);
+    const int rows = cvRound(input_image.rows * height_scaling);
+    const int cols = cvRound(input_image.cols * width_scaling);
+
     // Make a copy of the input image
 	cv::Mat output_image(rows, cols, CV_8UC1);
     int x = 0;
@@ -39,22 +43,22 @@ cv::Mat GeometricTransform::Resize(const cv::Mat& input_image, const double& hei
 }
 
 
-cv::Mat GeometricTransform::Rotate(const cv::Mat& input_image, const double& angle)
+cv::Mat GeometricTransform::Rotate(const cv::Mat& input_image, double angle)
 {
 
-    int image_height = input_image.rows;
-    int image_width = input_image.cols;
-    int half_image_height = image_height / 2;
-    int half_image_width = image_width / 2;
+    const int image_height = input_image.rows;
+    const int image_width = input_image.cols;
+    const int half_image_height = image_height / 2;
+    const int half_image_width = image_width / 2;
 
-	int output_image_size = static_cast<int>(sqrtf(pow(input_image.rows, 2) + pow(input_image.cols, 2)) + 0.5);
-    int half_output_size = output_image_size / 2;
+	const int output_image_size = static_cast<int>(sqrtf(pow(input_image.rows, 2) + pow(input_image.cols, 2)) + 0.5);
+    const int half_output_size = output_image_size / 2;
     // Make a copy of the input image
 	cv::Mat output_image = cv::Mat::zeros(output_image_size, output_image_size, CV_8UC1);
 
-	double angle_rad = angle * PI / 180;
-    double sin_angle = sin(angle_rad);
-    double cos_angle = cos(angle_rad);
+	const double angle_rad = angle * GeometricTransform::PI_ / 180;
+    const double sin_angle = sin(angle_rad);
+    const double cos_angle = cos(angle_rad);
 
 	int input_image_row = 0;
     int input_image_col = 0;
@@ -85,8 +89,9 @@ cv::Mat GeometricTransform::Rotate(const cv::Mat& input_image, const double& ang
 cv::Mat GeometricTransform::FlipLeftRight(const cv::Mat& input_image)
 {
 
-    int image_height = input_image.rows;
-    int image_width = input_image.cols;
+    const int image_height = input_image.rows;
+    const int image_width = input_image.cols;
+
     // Make a copy of the input image
     cv::Mat output_image = cv::Mat::zeros(image_height, image_width, CV_8UC1);
     // Loop over all pixels' intensities
@@ -103,8 +108,9 @@ cv::Mat GeometricTransform::FlipLeftRight(const cv::Mat& input_image)
 cv::Mat GeometricTransform::FlipUpDown(const cv::Mat& input_image)
 {
 
-    int image_height = input_image.rows;
-    int image_width = input_image.cols;
+    const int image_height = input_image.rows;
+    const int image_width = input_image.cols;
+
     // Make a copy of the input image
     cv::Mat output_image = cv::Mat::zeros(image_height, image_width, CV_8UC1);
     // Loop over all pixels' intensities
