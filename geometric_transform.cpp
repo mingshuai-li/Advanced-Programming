7efeddf9f7f6ad37edc591a6a787e0f05f77fb83
advancedlenard <ge26hab@mytum.de>
2021-12-25 19:47:56 +0800

    refactored some comments

--- geometric_transform.cpp
+++ geometric_transform.cpp
@@ -22,11 +22,12 @@ cv::Mat GeometricTransform::Resize(const cv::Mat& input_image, double height_sca
     const int rows = cvRound(input_image.rows * height_scaling);
     const int cols = cvRound(input_image.cols * width_scaling);
 
-    // Make a copy of the input image
+    // Makes a copy of the input image
 	cv::Mat output_image(rows, cols, CV_8UC1);
     int x = 0;
     int y = 0;
-    // Loop over all pixels' intensities
+
+    // Loops over all pixels' intensities
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
@@ -34,7 +35,7 @@ cv::Mat GeometricTransform::Resize(const cv::Mat& input_image, double height_sca
             x = static_cast<int>((i + 1) / height_scaling + 0.5) - 1;
             y = static_cast<int>((j + 1) / width_scaling + 0.5) - 1;
             output_image.at<uchar>(i, j) = input_image.at<uchar>(x, y);
-            // change the scale of the output image with given size
+            // changes the scale of the output image with given size
         }
     }
 
@@ -53,7 +54,8 @@ cv::Mat GeometricTransform::Rotate(const cv::Mat& input_image, double angle)
 
 	const int output_image_size = static_cast<int>(sqrtf(pow(input_image.rows, 2) + pow(input_image.cols, 2)) + 0.5);
     const int half_output_size = output_image_size / 2;
-    // Make a copy of the input image
+
+    // Makes a copy of the input image
 	cv::Mat output_image = cv::Mat::zeros(output_image_size, output_image_size, CV_8UC1);
 
 	const double angle_rad = angle * GeometricTransform::PI_ / 180;
@@ -62,15 +64,17 @@ cv::Mat GeometricTransform::Rotate(const cv::Mat& input_image, double angle)
 
 	int input_image_row = 0;
     int input_image_col = 0;
-    // Loop over all pixels' intensities
+    // Loops over all pixels' intensities
 	for (int i = 0; i < output_image_size; i++)
     {
 		for (int j = 0; j < output_image_size; j++)
 		{
-			// Move to the new coordinates relative to the center of rotation. The transformed image is multiplied by the original image transformation matrix
+			// Transforms to the new coordinates whose origin is at the center of rotation
+            // The transformed image is multiplied by the original image transformation matrix
             input_image_row = static_cast<int>((i - half_output_size) * cos_angle - (j - half_output_size) * sin_angle + 0.5);
 			input_image_col = static_cast<int>((i- half_output_size) * sin_angle + (j - half_output_size) * cos_angle + 0.5);
-            // Restore to the old coordinates relative to the origin of the upper left corne
+
+            // Transforms to the original coordinates whose origin is at the upper left corner
 			input_image_row += half_image_height;
 			input_image_col += half_image_width;
 
@@ -92,12 +96,12 @@ cv::Mat GeometricTransform::FlipLeftRight(const cv::Mat& input_image)
     const int image_height = input_image.rows;
     const int image_width = input_image.cols;
 
-    // Make a copy of the input image
+    // Makes a copy of the input image
     cv::Mat output_image = cv::Mat::zeros(image_height, image_width, CV_8UC1);
-    // Loop over all pixels' intensities
+    // Loops over all pixels' intensities
     for (int i = 0; i < image_height; i++)
         for (int j = 0; j < image_width; j++)
-            // Flip along Y axis
+            // Flips along Y axis
             output_image.at<uchar>(i, j) = input_image.at<uchar>(i, image_width - j -1);
 
     return output_image;
@@ -113,10 +117,10 @@ cv::Mat GeometricTransform::FlipUpDown(const cv::Mat& input_image)
 
     // Make a copy of the input image
     cv::Mat output_image = cv::Mat::zeros(image_height, image_width, CV_8UC1);
-    // Loop over all pixels' intensities
+    // Loops over all pixels' intensities
     for (int i = 0; i < image_height; i++)
         for (int j = 0; j < image_width; j++)
-            // Flip along X axis
+            // Flips along X axis
             output_image.at<uchar>(i, j) = input_image.at<uchar>(image_height - i - 1, j);
 
     return output_image;
